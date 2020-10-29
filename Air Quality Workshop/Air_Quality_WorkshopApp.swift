471e2e9ca828fcf2de9b71b06ac63ef16ed34f19
Sam Wright <sam.wright@purple.telstra.com>
2020-10-29 14:29:04 +1100

    Initial Commit

---
+++ Air Quality Workshop/Air_Quality_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_Quality_WorkshopApp.swift
+//  Air Quality Workshop
+//
+//  Created by Sam Wright on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Air_Quality_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Air Quality Workshop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Air Quality Workshop
+//
+//  Created by Sam Wright on 29/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
